8dd21f8fc8470f69a5a388d751f17e849cc52622
TumaIgor <tuea@rambler.ru>
2026-02-14 21:17:17 +0200

    2nd commit with add

---
+++ src/components/Main.jsx
@@ -0,0 +1,26 @@
+import friend1 from "../assets/friend1.jpg";
+import friend2 from "../assets/friend2.jpg";
+import friend3 from "../assets/friend3.jpg";
+import friend4 from "../assets/friend4.jpg";
+import friend5 from "../assets/friend5.jpg";
+import friend6 from "../assets/friend6.jpg";
+import friend7 from "../assets/friend7.jpg";
+import friend8 from "../assets/friend8.jpg";
+import friend9 from "../assets/friend9.jpg";
+
+import Hero from "./Hero.jsx";
+import DreamTeam from "./DreamTeam.jsx";
+import OpeningCrawl from "./OpeningCrawl.jsx";
+
+const Main = () => {
+    return (
+
+        <main className="clearfix">
+            <Hero />
+            <DreamTeam />
+            <OpeningCrawl />
+        </main>
+    );
+};
+
+export default Main;
